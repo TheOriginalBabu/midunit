e49ceb599d953ba57f9a7975266e663e2f151116
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2021-09-29 22:53:28 -0400

    good

--- creditcard.py
+++ creditcard.py
@@ -1,5 +1,31 @@
-input("Please enter credit card inf")
+import string
 
-f = open("ccinfo.txt", mode = "a")
-f.write("This text is written in python")
-input()
+
+file = open("ccinfo.txt", mode = "r+")
+
+dafile = file.read()
+#dafile = string(dafile)
+
+
+if bool(dafile) == True:
+    print("lol")
+    #SavedCCInfo = file.readlines(1)
+else:
+    print("ohno")
+input(dafile)
+
+# if bool(savedCCInfo) == True:
+#     print("Saved Credit Card information detected")
+#     print("Your saved credit card info is:",savedCCInfo)
+#     input()
+
+# elif bool(savedCCInfo) == False:
+#     while (int(ccInfo) - 1000000000000000) < 0:
+#         print("Please enter credit card info")
+#         ccInfo = input()
+#         if (int(ccInfo) - 1000000000000000) > 0:
+#             file.write(str(ccInfo))
+#         elif (int(ccInfo) - 1000000000000000) < 0:
+#             print("invalid cc number")
+
+
