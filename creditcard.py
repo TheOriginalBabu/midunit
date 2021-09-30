29fed89db1a81f3f87740ca356901afa6a1b77c7
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2021-09-30 10:08:45 -0400

    Update creditcard.py

--- creditcard.py
+++ creditcard.py
@@ -9,37 +9,42 @@ import msvcrt
 import random
 
 
-move = "0"
-inputmove ="0"
-aimove = random.choice(["rock","paper","scissors"])
-
+move = 0
+score = 0
+CONST_ACCEPT = [1,2,3]
 
 print("Welcome to Rock Paper Scissors!")
-while(inputmove not in [b"1", b"2", b"3"]):
-    print("Select your move (1 - Rock, 2 - Paper, 3 - Scissors)")
-    inputmove = msvcrt.getch()
-    if inputmove not in [b"1", b"2", b"3"]:
-        print("Invalid input.")
-        print()
 
-if inputmove == b"1":
-    move = "rock"
-elif inputmove == b"2":
-    move = "paper"
-elif inputmove == b"3":
-    move = "scissors"
+while(True):
+    while(move not in CONST_ACCEPT):
+        print("Select your move (1 - Rock, 2 - Paper, 3 - Scissors)")
+        move = int(msvcrt.getch())
+        if move not in CONST_ACCEPT:
+            print("Invalid input.")
+            print()
 
-print("You played",move,".")
-print("The AI played",aimove,".")
+    if move == 1:
+        move = "rock"
+    elif move == 2:
+        move = "paper"
+    elif move == 3:
+        move = "scissors"
 
-if move == "rock" and aimove == "paper" or move == "paper" and aimove == "scissors" or move == "scissors" and aimove == "rock":
-    print("You lost.")
-elif move == aimove:
-    print("You tied with the A.I.")
-elif move == "rock" and aimove == "scissors" or move == "paper" and aimove == "rock" or move == "scissors" and aimove =="paper":
-    print("You won!")
+    aimove = random.choice(["rock","paper","scissors"])
 
-print(move,aimove)
+    print("You played",move,".")
+    print("The AI played",aimove,".")
+
+    if move == "rock" and aimove == "paper" or move == "paper" and aimove == "scissors" or move == "scissors" and aimove == "rock":
+        print("You lost.")
+    elif move == aimove:
+        print("You tied with the A.I.")
+    elif move == "rock" and aimove == "scissors" or move == "paper" and aimove == "rock" or move == "scissors" and aimove =="paper":
+        score = score + 1
+        print("You won!")
+        print("Your new score is",score)
+
+
 
 
 #file = open("ccinfo.txt", mode = "r+")
